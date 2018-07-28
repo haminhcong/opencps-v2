908e19443effad7213f2a85dc6ff3eb3949456cc
haminhcong <haminhcongbkhn@gmail.com>
2018-07-28 20:19:24 +0700

    add new test

---
+++ modules/backend-auth/src/test/java/org/opencps/auth/utils/APIDateTimeUtilsTest.java
@@ -0,0 +1,18 @@
+package org.opencps.auth.utils;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
+
+public class APIDateTimeUtilsTest {
+    @Before
+    public void setUp() {
+    }
+
+    @Test
+    public void convertNormalDateToLuceneDateTest() {
+        String outputDateString = APIDateTimeUtils.convertNormalDateToLuceneDate(null);
+        Assert.assertEquals(outputDateString, "");
+    }
+}
