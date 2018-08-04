9307f51155dd4e8ef93ebc5096824ca656d2a7e7
haminhcong <haminhcongbkhn@gmail.com>
2018-08-04 12:18:44 +0700

    check concurrency

--- concurrency.sh
+++ concurrency.sh
@@ -3,10 +3,16 @@ echo "test" >> test.txt
 git add .
 git commit -m "check concurrency"
 git push
+
+sleep 3
+
 echo "test" >> test.txt
 git add .
 git commit -m "check concurrency"
 git push
+
+sleep 3 
+
 echo "test" >> test.txt
 git add .
 git commit -m "check concurrency"
