baa94fd1d54b85c45d18c9fb61ffb2e42b2128fa
haminhcong <haminhcongbkhn@gmail.com>
2018-08-07 00:51:42 +0700

    update tests

--- modules/backend-auth/src/test/java/org/opencps/auth/utils/APIDateTimeUtilsTest.java
+++ modules/backend-auth/src/test/java/org/opencps/auth/utils/APIDateTimeUtilsTest.java
@@ -13,8 +13,20 @@ public class APIDateTimeUtilsTest {
 
     
     @Test
-    public void convertNormalDateToLuceneDateTest() {
+    public void convertNormalDateToLuceneDateTestNull() {
         String outputDateString = APIDateTimeUtils.convertNormalDateToLuceneDate(null);
+        System.out.println(outputDateString);
+        Assert.assertEquals(outputDateString, "");
+    }
+    @Test
+    public void convertNormalDateToLuceneDateTestNormal() {
+        String outputDateString = APIDateTimeUtils.convertNormalDateToLuceneDate("22/08/2011");
+        Assert.assertEquals(outputDateString, "20110822");
+    }
+
+    @Test
+    public void convertNormalDateToLuceneDateTestIncorrectFormat() {
+        String outputDateString = APIDateTimeUtils.convertNormalDateToLuceneDate("2011/08/20");
         Assert.assertEquals(outputDateString, "");
     }
 }
